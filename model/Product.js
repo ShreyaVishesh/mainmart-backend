328ed93586d002624bcaa1be7bd4931200eb57fe
hackerraushan <rnkp755@gmail.com>
2024-04-12 17:19:55 +0530

    Update Product model schema

--- model/Product.js
+++ model/Product.js
@@ -1,33 +1,35 @@
 const mongoose = require('mongoose');
-const {Schema} = mongoose;
+const { Schema } = mongoose;
 
 
 const productSchema = new Schema({
-    title: { type : String, required: true, unique: true},
-    description: { type : String, required: true},
-    price: { type: Number, min:[1, 'wrong min price'], max:[10000, 'wrong max price']},
-    discountPercentage: { type: Number, min:[1, 'wrong min discount'], max:[99, 'wrong max discount']},
-    rating: { type: Number, min:[0, 'wrong min rating'], max:[5, 'wrong max price'], default:0},
-    stock: { type: Number, min:[0, 'wrong min stock'], default:0},
-    brand: { type : String, required: true},
-    range: { type : String},
-    category: { type : String, required: true},
-    subCategory: { type : String},
-    video: { type : String},
-    thumbnail: { type : String, required: true},
-    images:{ type : [String], required: false},
-    deleted: { type : Boolean, default: false},
+    title: { type: String, required: true, unique: true },
+    description: { type: String, required: true },
+    price: { type: Number, min: [1, 'wrong min price'], max: [10000, 'wrong max price'] },
+    discountPercentage: { type: Number, min: [1, 'wrong min discount'], max: [99, 'wrong max discount'] },
+    rating: { type: Number, min: [0, 'wrong min rating'], max: [5, 'wrong max price'], default: 0 },
+    stock: { type: Number, min: [0, 'wrong min stock'], default: 0 },
+    brand: { type: String, required: true },
+    range: { type: String },
+    category: { type: String, required: true },
+    subCategory: { type: String },
+    color: { type: [String] },
+    size: { type: [String] },
+    video: { type: String },
+    thumbnail: { type: String, required: true },
+    images: { type: [String], required: false },
+    deleted: { type: Boolean, default: false },
 })
 
-const virtual  = productSchema.virtual('id');
-virtual.get(function(){
+const virtual = productSchema.virtual('id');
+virtual.get(function () {
     return this._id;
 })
-productSchema.set('toJSON',{
+productSchema.set('toJSON', {
     virtuals: true,
     versionKey: false,
-    transform: function (doc,ret) { delete ret._id}
+    transform: function (doc, ret) { delete ret._id }
 })
 
 
-exports.Product = mongoose.model('Product',productSchema)
+exports.Product = mongoose.model('Product', productSchema)
